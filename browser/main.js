06ea5a33c096831a4e293b5c1537388f80844349
Parapass <59424861+Parapass@users.noreply.github.com>
2021-08-16 04:02:15 -0400

    cash money

--- browser/main.js
+++ browser/main.js
@@ -1,18 +1,23 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require('electron');
+const nativeImage = require('electron').nativeImage;
+app.commandLine.appendSwitch('enable-transparent-visuals');
+
+
 
 function createWindow () {
   // Create the browser window.
   let win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1280,
+    height: 720,
     webPreferences: {
-      nodeIntegration: true;
-      frame: false
+      frame: true
     }
   })
   
   win.removeMenu()
   win.loadURL('https://modelsaber.com');
+  win.setIcon('icon.ico');
 }
 
 app.whenReady().then(createWindow)
+
